4f2c0bdae90c2d6743af82158e7a687922907ee7
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-06-21 17:18:43 +0700

    fic bug chart component

--- components/performence/Chart.tsx
+++ components/performence/Chart.tsx
@@ -1,15 +1,17 @@
 "use client";
-import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from "recharts";
-import { Card } from "../ui/card";
+
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import { Card } from "@/components/ui/card";
 
 const Chart = ({ data }: { data: { name: string; total: number }[] }) => {
   return (
     <Card>
-      <ResponsiveContainer width={"100%"} height={400}>
+      <ResponsiveContainer width="100%" height={400}>
         <BarChart data={data}>
           <XAxis dataKey="name" stroke="888888" fontSize={12} axisLine={false} tickLine={false} />
-          <YAxis dataKey="name" stroke="888888" fontSize={12} axisLine={false} tickLine={false} tickFormatter={(value) => `${{ value }}`} />
-          <Bar dataKey="total" barSize={30} fill="#8884d8" radius={[4, 4, 0, 0]} />
+          <YAxis stroke="888888" fontSize={12} axisLine={false} tickLine={false} tickFormatter={(value) => `$${value}`} />
+          <Tooltip />
+          <Bar dataKey="total" fill="#8884d8" radius={[4, 4, 0, 0]} />
         </BarChart>
       </ResponsiveContainer>
     </Card>
